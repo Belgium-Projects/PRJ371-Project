9c8eeb7fdec093653e91276dd545fb36e7e0dab4
DayDreamerDevs <rynoreyneke54@gmail.com>
2023-10-16 18:05:40 +0200

    [Unity] Added Car Tag & Maintenance Beacon Logic

--- PRJ371-Unity-Project/Assets/Scripts/Infrastructure System/RoadMaintenanceBeacon.cs
+++ PRJ371-Unity-Project/Assets/Scripts/Infrastructure System/RoadMaintenanceBeacon.cs
@@ -1,28 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public class RoadMaintenanceBeacon : MonoBehaviour
 {
-    // Radius within which cars should be warned
-    public float warningRadius = 50.0f;
+    //Editor variables
+    [SerializeField] private float warningRadius = 50.0f;
+    [SerializeField] private GameObject beaconObj;
+
+    //Global variables
     private SphereCollider beaconCollider;
+    private ArduinoCarSimulator carSimulator;
     private void Start()
     {
-        // Set up the beacon's trigger zone
-        beaconCollider = gameObject.AddComponent<SphereCollider>();
-        beaconCollider.isTrigger = true;
-        beaconCollider.radius = warningRadius;
+        //Get the beacon sphere collider and Arduino script
+        beaconCollider = beaconObj.GetComponent<SphereCollider>();
+        carSimulator = gameObject.GetComponent<ArduinoCarSimulator>();
+
+        //Check if objects exist in the scene
+        if (beaconCollider == null )
+        {
+            Debug.LogError("No sphere collider added to the game object");
+        }
+        if (carSimulator == null)
+        {
+            Debug.LogError("No ArduinoCarSimilator script in the scene");
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object entering the trigger zone is a car
+        //Check if the collider entered is the car
         if (other.CompareTag("Car"))
         {
-            // Send a warning to the car to slow down
-            ArduinoCarSimulator carSimulator = other.GetComponent<ArduinoCarSimulator>();
-            if (carSimulator != null)
-            {
-                carSimulator.ReceiveApiRequest("SlowDown");
-            }
+            //Send a warning to the car to slow down  
+            Debug.Log("Slow Down");
+            //carSimulator.ReceiveApiRequest("SlowDown");
         }
     }
 }
